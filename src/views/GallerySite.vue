4f3e3498418d3552258f762cf8099c79cf41883b
O.Cherkasov <6138210@gmail.com>
2023-05-01 11:23:07 +0300

    next

--- src/views/GallerySite.vue
+++ src/views/GallerySite.vue
@@ -1,28 +1,44 @@
 <template>
-  <div class="">
+  <div class="gallery">
     <DoubleTitle title="Gallery" />
-    <div
-      class=""
-      v-for="(image, index) in images"
-      :key="index"
-      :index="index + 1"
-    >
+    <div class="gallery__img">
       <img
+        v-for="index in amountImag"
+        :key="index"
+        :index="index + 1"
         :src="require(`@/assets/gallery/gallery${index}.png`)"
         alt="foto repair"
+        height="282"
       />
     </div>
   </div>
 </template>
-
 <script>
 import DoubleTitle from "@/components/DoubleTitle.vue";
 
 export default {
   name: "GallerySite",
   components: { DoubleTitle },
+  data() {
+    return {
+      amountImag: 8,
+    };
+  },
 };
 </script>
 
 <style>
+.gallery {
+  margin-top: 40px;
+}
+
+.gallery__img {
+  margin: 0 auto;
+  max-width: 1192px;
+  margin-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
 </style>
